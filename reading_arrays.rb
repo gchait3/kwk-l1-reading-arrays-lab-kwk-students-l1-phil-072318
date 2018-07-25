6b00f21a6817d8a47971a78b558f8d197b9d246c
Grace T <gtsai64@gmail.com>
2018-07-25 14:31:36 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,11 +8,11 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
-  student_names=["Adele","Beyoncé","Cardi", "Lady Gaga", "Nicki Minaj", "Rihanna"]
-  puts student_names.first
-end
-first_student_by_index
+#   # Write a solution that returns the first student in the array student_names
+#   student_names=["Adele","Beyoncé","Cardi", "Lady Gaga", "Nicki Minaj", "Rihanna"]
+#   puts student_names.first
+# end
+# first_student_by_index
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
